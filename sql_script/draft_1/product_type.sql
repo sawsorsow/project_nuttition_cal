daad1a5ba964fc5a31cf4d522003841bb53e28c2
Kantinun Aundee <singasxng@gmail.com>
2025-06-26 13:05:22 +0700

    draft_1 html

--- sql_script/draft_1/product_type.sql
+++ sql_script/draft_1/product_type.sql
@@ -10,18 +10,17 @@ create table if not exists product_type (
 	product_type_eng varchar(64),
     product_subtype_eng varchar(64),
     product_type_thai varchar(128),
-    product_subtype_thai varchar(128),
+    product_subtype_thai varchar(256),
     
     -- serving
     serving_size varchar(16)
 );
 
 insert into product_type
-values ('445', 'Dairy product and substitutes', 'Milk, milk-substitute beverages, milk-based drinks', 'กลุ่มผลิตภัณฑ์นมและผลิตภัณฑ์ที่ใช้แทนนม', 'นม  ผลิตภัณฑ์นม และใช้เครื่องดื่มที่มีนมเป็นส่วนประกอบหลัก ชนิดพร้อมดื่ม', '200 ml')
--- ('445', 'Dairy product and substitutes', 'Milk, milk-substitite beverages, milk-based')
-
-
-
+values ('445', 'Dairy product and substitutes', 'Milk, milk-substitute beverages, milk-based drinks', 'กลุ่มผลิตภัณฑ์นมและผลิตภัณฑ์ที่ใช้แทนนม', 'นม  ผลิตภัณฑ์นม และใช้เครื่องดื่มที่มีนมเป็นส่วนประกอบหลัก ชนิดพร้อมดื่ม', '200 ml'),
+('445', 'Dairy product and substitutes', 'Milk, milk-substitite beverages, milk-based drink, powder', 'กลุ่มผลิตภัณฑ์นมและผลิตภัณฑ์ที่ใช้แทนนม', 'นม ผลิตภัณฑ์นม และเครื่องดื่มที่ มีนม เป็นส่วนประกอบหลัก ชนิดแห้ง', '200 ml'),
+('445', 'Dairy product and substitutes', 'Yoghurt, cream', 'กลุ่มผลิตภัณฑ์นมและผลิตภัณฑ์ที่ใช้แทนนม', 'โยเกิร์ตชนิดครีม', '150 grams'),
+('445', 'Dairy product and substitutes', 'Yoghurt, frozen', 'กลุ่มผลิตภัณฑ์นมและผลิตภัณฑ์ที่ใช้แทนนม', 'โยเกิร์ตแช่แข็ง', '80 grams')
 ;
 
 select * from product_type;
